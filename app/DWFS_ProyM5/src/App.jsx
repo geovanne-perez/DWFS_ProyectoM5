6d7abf1f5235f8bb4d40145874c4e8d8bfbd66e2
Geovanne Perez <geovanne.perez@outlook.com>
2024-03-12 10:55:28 -0600

    Concluido funcionamiento de cambio de componentes con botones

--- app/DWFS_ProyM5/src/App.jsx
+++ app/DWFS_ProyM5/src/App.jsx
@@ -17,7 +17,8 @@ function App() {
     <>
       <div className='container-fluid bg-dark'>
           <h1 className='text-primary display-5 p-3'>DWFS - M5 - Proyecto React</h1>
-          <p className='text-light pt-2 pb-4 p-3'>Alumno: José Geovanne Pérez Meza </p>
+          <p className='text-light pt-2 pb-1 p-3'>Alumno: José Geovanne Pérez Meza </p>
+          <p className='text-light pt-0 pb-4 p-3'>Esta app carga datos de la API de Chess.com, y los muestra usando dos componentes de React. Para el front-end se utilizaron componentes Bootstrap.</p>
       </div>
       <Header headerSelButton = {handleHeaderButtons}/>
       {btnSel === 'Show Rank' ? <ChessTableRank/> : <ChessTableWinCount/>}
